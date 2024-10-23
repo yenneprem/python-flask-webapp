4458200a21ee3d4c099a687776816be7e88e18ce
prem yenne <yennepremkumar@gmail.com>
2024-10-23 23:17:50 +0530

    first commit

---
+++ javaapps/yenne/src/main/java/com/prem/yenne/App.java
@@ -0,0 +1,13 @@
+package com.prem.yenne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
